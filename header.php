bfb0af8658c12bba23649141e419420312fd419a
David FLAUGERE <flaugere.david@gmail.com>
2018-03-08 10:29:21 +0100

    fix(header): Surcharge header suite maj master understrap

---
+++ header.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package understrap
+ */
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<div class="hfeed site" id="page">
+
+	<!-- ******************* The Navbar Area ******************* -->
+	<div class="wrapper-fluid wrapper-navbar" id="wrapper-navbar">
+
+		<a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content',
+		'understrap' ); ?></a>
+
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+
+		<?php if ( 'container' == $container ) : ?>
+			<div class="container" onclick="if (jQuery('.navbar-toggler').is(':visible')) {jQuery('.navbar-collapse').toggle().toggleClass('in');}">
+		<?php endif; ?>
+
+					<!-- Your site title as branding in the menu -->
+					<?php if ( ! has_custom_logo() ) { ?>
+
+						<?php if ( is_front_page() && is_home() ) : ?>
+
+							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+							
+						<?php else : ?>
+
+							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+						
+						<?php endif; ?>
+						
+					
+					<?php } else {
+						the_custom_logo();
+					} ?><!-- end custom logo -->
+
+				<button class="navbar-toggler" type="button">
+                                    <i class="fa fa-bars" aria-hidden="true"></i> Menu
+                                </button>
+				<!-- The WordPress Menu goes here -->
+				<?php wp_nav_menu(
+					array(
+						'theme_location'  => 'primary',
+						'container_class' => 'collapse navbar-collapse justify-content-end',
+						'container_id'    => 'navbarNavDropdown',
+						'menu_class'      => 'navbar-nav',
+						'fallback_cb'     => '',
+						'menu_id'         => 'main-menu',
+						'walker'          => new understrap_WP_Bootstrap_Navwalker(),
+					)
+				); ?>
+			<?php if ( 'container' == $container ) : ?>
+			</div><!-- .container -->
+			<?php endif; ?>
+
+		</nav><!-- .site-navigation -->
+		<nav class="navbar navbar-custom site-navigation navbar-toggleable-md" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
+                    <div class="container">
+                        <?php wp_nav_menu(
+                                array(
+                                    'menu' => 'Menu blog',
+                                    'theme_location' => 'sub-primary',
+                                    'container_class' => 'collapse navbar-collapse justify-content-end',
+                                    'menu_class' => 'nav navbar-nav',
+                                    'fallback_cb' => '',
+                                    'menu_id' => 'sub-main-menu',
+                                    'walker' => new understrap_WP_Bootstrap_Navwalker()
+                                )
+                        ); ?>
+                    </div>
+                </nav>
+
+	</div><!-- .wrapper-navbar end -->
